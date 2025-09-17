1af5451c54c9f3bfaa5b18b131522e46090eaea7
kshitijofficial <srivastavakshitijprofessional@gmail.com>
2025-09-17 08:15:31 +0530

    Remove wrongly named file

--- src/services/chaingConfig.js
+++
@@ -1,5 +0,0 @@
-export const CHAINS = {
-    1: { name: 'Ethereum Mainnet', rpc: 'https://shape-mainnet.g.alchemy.com/v2/xhB6Ea_CdRJ9mt7_gExxwayO0v5qAPdP' },
-    11155111: { name: 'Sepolia', rpc: 'https://shape-sepolia.g.alchemy.com/v2/xhB6Ea_CdRJ9mt7_gExxwayO0v5qAPdP' },
-    80002: { name: 'Polygon Amoy', rpc: 'https://polygon-amoy.g.alchemy.com/v2/xhB6Ea_CdRJ9mt7_gExxwayO0v5qAPdP' }
-};
